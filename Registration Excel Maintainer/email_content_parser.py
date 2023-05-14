26b83dffc19e3589bd3e351cf6e6417e5407e9c6
GameTime With Aryan <gametimewitharyan@gmail.com>
2023-05-15 00:09:12 +0530

    V1 Completed
    First completely working verison of this project completed

---
+++ Registration Excel Maintainer/email_content_parser.py
@@ -0,0 +1,181 @@
+"""
+Works with google account password only when less secure apps are enabled
+imap = imaplib.IMAP4_SSL(imap_server)
+imap.login(email_address, password)
+
+Check Gmail API docs for more info
+message content is usaually encoded in base64 and quoted printable text encoding
+
+Algorithm for getting name, email, phone number, city -
+Seperate the string at \r\n
+Get the index of name from list
+The next index will be the value of name, next will be email...... DONE!
+
+Word meaning:
+message - complete message response, check Gmail API docs for more info
+message resource - a response containing info about message id etc. but not the content, obtained from labels.list
+message content - message body
+"""
+
+import base64
+from datetime import date, timedelta
+import quopri
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class GmailReader:
+    def __init__(self, service):
+        # Google service resource for interacting with Gmail API
+        self.service = service
+
+    def get_label_id(self, label_name: str):
+        label_id = None
+        # Fetching list of all labels
+        label_result: dict = self.service.users().labels().list(userId="me").execute()
+        labels: list[dict] = label_result.get("labels")
+
+        # Finding label id
+        for label in labels:
+            if label.get("name") == label_name:
+                label_id = label.get("id")
+                break
+        return label_id
+
+    def get_msg_resources_under_label(self, label_ids: list[str] | str, custom_filter: str = ""):
+        # Using label id to get all message resources from that label
+        msg_resources_result: dict = self.service.users().messages().list(userId="me", labelIds=label_ids,
+                                                                          q=custom_filter).execute()
+        msg_resources: list[dict] = msg_resources_result.get("messages")
+        return msg_resources
+
+    def get_msg_from_resource(self, resource: dict):
+        message_id = resource.get("id")
+        message_result: dict = self.service.users().messages().get(userId="me", id=message_id).execute()
+        return message_result
+
+    @staticmethod
+    def decode_msg_content(content: str):
+        """
+        Decodes the encoded content of message given by Gmail API
+        First decodes the base64 encoding
+        Secondly decodes the Quoted Printable encoding
+        """
+
+        # Step 1 - Decoding the base64 text
+        b64_decoded_message_content: bytes = base64 \
+            .urlsafe_b64decode(content)
+        # Step 2 - Decoding the quoted printable text, and converting bytes to str
+        quopri_decoded_message_content: str = quopri \
+            .decodestring(b64_decoded_message_content) \
+            .decode(errors="replace")
+        return quopri_decoded_message_content
+
+
+def parse_content(content: str):
+    """Check module docstring for details on how the alogrithm works
+
+    Returns
+    -------
+    list[name: str, email: str, phone: str, city: str] : a single entry list
+    """
+
+    parsed_details = []
+    lines_in_content = content.split("\r\n")
+    name_field_index = lines_in_content.index("*Name*")
+    for index in range(0, 4):
+        parsed_details.append(lines_in_content[name_field_index + 1 + (index * 2)])
+    return parsed_details
+
+
+def get_after_date_filter(n_days):
+    """Returns today's date - n_days
+    To be used in the filter for getting all the emails after a given date
+    Return format:
+    \"year/month/day\"
+    """
+    today = date.today()
+    after_date = today - timedelta(days=n_days)
+    after_date_filter = f"after:{after_date.year}/{after_date.month}/{after_date.day}"
+    return after_date_filter
+
+
+def get_credentials():
+    scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def get_entry_details(n_days=1):
+    """
+
+    Parameters
+    ----------
+     n_days : int
+        Get entry details for all the emails received within last n days
+
+    Returns
+    -------
+    list[list] : a list of all entries
+
+    """
+
+    all_entry_detais: list = []
+    test_label_name: str = "Test, using quora"
+    custom_filter = get_after_date_filter(n_days)
+
+    # Get credentials for authentication
+    creds = get_credentials()
+    # Building Gmail API Caller
+    service = build('gmail', 'v1', credentials=creds)
+    gmail_reader = GmailReader(service)
+
+    # Getting test label id
+    test_label_id = gmail_reader.get_label_id(test_label_name)
+    # Getting all message resources under test label
+    message_resources: list[dict] = gmail_reader.get_msg_resources_under_label(test_label_id, custom_filter)
+
+    if message_resources is None:
+        print(f"No Form Entry Emails in last {n_days} days")
+        return []
+
+    # Getting message content of each message resource
+    for index, msg_resources in enumerate(message_resources):
+        # Getting each message
+        message = gmail_reader.get_msg_from_resource(msg_resources)
+        # Extracting content of message, encoded in base64 and quoted printable text encoding
+        encoded_message_content: str = message.get("payload").get("parts")[0].get("body").get("data")
+        # Decoding message content
+        decoded_message_content = gmail_reader.decode_msg_content(encoded_message_content)
+        # Parsing content
+        entry_details = parse_content(decoded_message_content)
+        all_entry_detais.append(entry_details)
+    return all_entry_detais
+
+
+# ENTRY LIST FIELD INDEXES
+
+
+if __name__ == '__main__':
+    print(get_entry_details(1))
--- Registration Excel Maintainer/excel_sheets_maintainer.py
+++ Registration Excel Maintainer/excel_sheets_maintainer.py
@@ -1,1 +1,36 @@
-# This file will be used to maintain the excel sheet using the data from gmail
+from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from email_content_parser import get_entry_details
+
+# CONSTANTS
+NAME_COLUMN = "A"
+EMAIL_COLUMN = "B"
+PHONE_COLUMN = "C"
+CITY_COLUMN = "D"
+ENTRY_FIELD_COLUMNS = (NAME_COLUMN, EMAIL_COLUMN, PHONE_COLUMN, CITY_COLUMN)
+
+# Loading excel workbook
+filename = "Form Entries.xlsx"
+workbook = load_workbook(filename)
+worksheet: Worksheet = workbook.worksheets[0]
+
+list_starting_index = 0
+form_entries: list = get_entry_details(2)
+
+last_updated_email: str = worksheet[f"{EMAIL_COLUMN}{worksheet.max_row}"].value
+# Getting index from which entry to start adding in the worksheet
+# If the last updated name is in the list, then start adding entries
+# from the entry after the last updated entry, identified by its email
+# If the last updated entry is not in the list, then add all the entries
+for index, entry in enumerate(form_entries):
+    if entry[1] == last_updated_email:
+        list_starting_index = index + 1
+
+last_row: int = worksheet.max_row  # Last updated row
+# Adding each entry to excel worksheet
+for entry_index, entry in enumerate(form_entries[list_starting_index:]):
+    for field_index, field in enumerate(entry):
+        cell_coordinate = f"{ENTRY_FIELD_COLUMNS[field_index]}{last_row + entry_index + 1}"
+        worksheet[cell_coordinate] = entry[entry_index]
+
+workbook.save(filename=filename)
